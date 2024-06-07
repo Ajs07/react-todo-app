023a9c332212f33dc2ea0cf0d89cb83f8fd887b2
Ajs07 <arnoldjulian24@gmail.com>
2024-06-07 21:53:29 +0700

    Add CSS to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Todos from './components/Todos'
 
 function App() {
     /* todos merupakan variable state dan pengaturan nilai awal 0 bisa dilakukan dengan code useState(0). 
@@ -22,16 +23,14 @@ function App() {
     },
   ]) 
 
-  console.log(todos)
-
   return (
-    <div>
-      <h1>My Todos List</h1>
-      {/* Method map diperlukan untuk mengambil data dari sebuah Array satu per satu, gunakan method pada {}*/}
-      {todos.map((todo) => {
-        // key={todo.id} seharusnya ditambahkan pada masing-masing elemen dalam sebuah array untuk memberikan identitas yang unik dan membantu React mendeteksi element mana yang telah di-render.
-        return <p key={todo.id}>{todo.title}</p>
-      })}
+    // Menambahkan CSS
+    /*Perlu menuliskan kurung kurawal 2x seperti <div style={{CSS code}} sebab code JSX itu sendiri memerlukan {} dan sebuah object {CSS code} ditambahkan di dalamnya.
+    Property-property CSS sangat mirip dengan CSS yang normal namun dalam JSX ini berbeda. Contohnya, property text-align dalam CSS adalah textAlign dalam JSX (hubungkan 2 kata dengan sebuah huruf kapital, ini disebut Camel Case) dan padding: 12px; adalah padding: '12px', (apit nilainya dengan tanda petik). */
+    <div style={{textAlign: 'center', padding: '12px'}}>
+      <h1 style={{fontSize:'36px'}}>My Todos List</h1>
+      {/*Hanya memanggil komponen Todos dan berikan datanya */}
+      <Todos todos={todos}/>
     </div>
   )
 }
